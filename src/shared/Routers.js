7da6ffbc9f6511a086a1140244c6ceb4f3139d24
Skelman50 <skelman50@gmail.com>
2020-01-18 22:47:27 +0200

    update routers

--- src/shared/Routers.js
+++ src/shared/Routers.js
@@ -2,13 +2,19 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import HomePage from "../pages/home/Home";
+import Messages from "../pages/messages/Messages";
 
 const Routers = () => {
   return (
     <BrowserRouter>
       <Navbar />
       <Switch>
-        <Route path="/" component={HomePage} />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/bio" component={HomePage} />
+        <Route exact path="/pics" component={HomePage} />
+        <Route exact path="/companies" component={HomePage} />
+        <Route exact path="/links" component={HomePage} />
+        <Route exact path="/messages" component={Messages} />
       </Switch>
     </BrowserRouter>
   );
