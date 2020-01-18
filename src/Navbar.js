ce9fecd73191fe005441b14f089b62bbc917a8b7
Skelman50 <skelman50@gmail.com>
2020-01-18 09:43:32 +0200

    web-workers-example

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,55 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import { Menu, Segment } from "semantic-ui-react";
-import { Link } from "react-router-dom";
-import { withRouter } from "react-router";
+import { NavLink } from "react-router-dom";
 
-class Navbar extends Component {
-  state = { activeItem: null };
-  componentDidMount() {
-    const activeItem = this.getlocation();
-    console.log(activeItem);
-    this.setState({ activeItem });
-  }
+const Navbar = () => {
+  return (
+    <Segment inverted>
+      <Menu inverted pointing secondary className="menu">
+        <Menu.Item as={NavLink} exact to="/" name="home" />
+        <Menu.Item as={NavLink} to="/messages" name="messages" />
+        <Menu.Item as={NavLink} to="/friends" name="friends" />
+      </Menu>
+    </Segment>
+  );
+};
 
-  getlocation = () => {
-    const { pathname } = this.props.location;
-    if (pathname === "/") return "home";
-    return pathname.slice(1, pathname.length);
-  };
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
-
-  render() {
-    const { activeItem } = this.state;
-    return (
-      <Segment inverted>
-        <Menu inverted pointing secondary className="menu">
-          <Menu.Item
-            as={Link}
-            to="/"
-            name="home"
-            active={activeItem === "home"}
-            onClick={this.handleItemClick}
-          />
-
-          <Menu.Item
-            as={Link}
-            to="/messages"
-            name="messages"
-            active={activeItem === "messages"}
-            onClick={this.handleItemClick}
-          />
-
-          <Menu.Item
-            as={Link}
-            to="/friends"
-            name="friends"
-            active={activeItem === "friends"}
-            onClick={this.handleItemClick}
-          />
-        </Menu>
-      </Segment>
-    );
-  }
-}
-
-export default withRouter(Navbar);
+export default Navbar;
